28a60fdd4d99fe27831190aa3f494840774b95db
Abdurahmon <abdurahmonikromov634@gmail.com>
2024-11-01 21:15:38 +0500

    homework

---
+++ main.js
@@ -0,0 +1,154 @@
+// 1-MASALA
+
+// var n = prompt("N sonini kiriting");
+// for (let i = 1; i <= n; i++) {
+//   console.log(i);
+// }
+
+
+
+// 2-MASALA
+
+// var n = prompt("N sonini kiriting");
+// for (let i = 1; i <= n; i++) {
+//   if(i % 15 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3-MASALA
+
+// var n = prompt("N sonini kiriting");
+// for (let i = 1; n >= i; n--) {
+//     console.log((n) - 0);
+// }
+
+
+
+// 4-MASALA
+
+// let a = parseInt(prompt("A kiriting"))
+// let b = parseInt(prompt("b kiriting"))
+
+// let count = 0
+// let sum = 0
+
+// if(a > b) {
+//   let change = a         //bu masala negadir promptda ishlamadi
+//   a = b
+//   b = change
+// }
+
+// for (let i = a + 1; i <= b; i++) {
+//     sum += i
+//     count++
+// }
+// console.log(sum / 1);
+// console.log(count - 0);
+
+
+
+//5-MASALA
+
+// let a = parseInt(prompt("A kiriting"))
+// let b = parseInt(prompt("B kiriting"))   
+
+
+// if(a > b) {
+//   let change = a         //bu masala negadir promptda ishlamadi
+//   a = b
+//   b = change
+// }
+
+// for (let i = a + 1; i <= b; i++) {
+//     if(b % i == 0) {
+//       console.log(i);
+//     }
+// }
+
+
+// 6-MASALA
+
+// let a = parseInt(prompt("A kiriting"));
+// let b = parseInt(prompt("B kiriting"));
+
+// let total = 0;
+
+// if (a > b) {
+//     let change = a;
+//     a = b;                                    //bu masala negadir promptda ishlamadi
+//     b = change;
+//   }
+// for (let i = a + 1; i < b; i++) {
+//     total += i;
+// }
+
+// console.log(total);
+
+
+// 7-MASALA
+
+
+// let res = 1
+
+// for (let i = 5 + 1; i <= 49; i++) {
+//   if(i % 2 != 0) {
+//     res *= i
+//   }
+// }
+
+// console.log(BigInt(res));
+
+//8-MASALA
+
+// let a = prompt("A kesmani kiriting")
+// let b = prompt("B kesmani kiriting")
+
+// if(a > b) {
+//   let change = a       
+//   a = b
+//   b = change
+// }
+
+// let res = Math.floor(a / b)
+// console.log(res);
+
+
+
+//9-MASALA
+
+// let a = prompt("A kesmani kiriting")
+// let b = prompt("B kesmani kiriting")
+
+// if(a > b) {
+//   let change = a       
+//   a = b
+//   b = change
+// }
+
+// let res = (a % b)
+// console.log(res);
+
+
+
+//10-MASALA
+
+// let price = prompt("Narxni kiriting")
+// let weight = prompt("Og'irlikni kiriting")
+
+// let res = price * weight
+// alert(res + " so'm")
+
+//11-MASALA
+
+// let num = prompt("Ikki xonali son kiriting:")
+// let a = Math.floor(num / 10) 
+// let b = num % 10
+
+// let count = 0
+
+// if (a > b) {
+//   for (let i = b + 1; i <= a; i++) {
+//     count + i
+//   }
+// }
